5a4250dcad7a6d6a38e179b4b01504073b68950d
xiaoZ857 <786187157@qq.com>
2025-12-12 01:22:58 +0800

    优化提示词参数组2编译结果上传

--- results/llama3_2_3b_0shot_temp0_8_topP0_95_promptv1/Prob038_count15/Prob038_count15_sample01.sv
+++ results/llama3_2_3b_0shot_temp0_8_topP0_95_promptv1/Prob038_count15/Prob038_count15_sample01.sv
@@ -4,6 +4,6 @@ module TopModule (
   input reset,
   output reg [3:0] q
 );
-assign q[3:0] = ( ~reset & clk ) ? {15'b1111} : ( ~q + 1'b1 );
 
 endmodule
+
--- results/llama3_2_3b_0shot_temp0_8_topP0_95_promptv1/Prob144_conwaylife/Prob144_conwaylife_sample01.sv
+++ results/llama3_2_3b_0shot_temp0_8_topP0_95_promptv1/Prob144_conwaylife/Prob144_conwaylife_sample01.sv
@@ -5,6 +5,5 @@ module TopModule (
   input [255:0] data,
   output reg [255:0] q
 );
-assign q = (load) ? data : 
-    {q[15:1],{q[31:16],q[47:17]}};
+
 endmodule
